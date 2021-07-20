9202e2dbbacd556e31d2423b18270b74b284ab67
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-07-20 18:04:09 +0200

    Feat: Making Cards and First Class

---
+++ app/src/main/java/com/example/dev/Item.java
@@ -0,0 +1,12 @@
+package com.example.dev;
+
+public class Item {
+    private String mTitle;
+
+    public Item(String title){
+        mTitle = title;
+    }
+    public String getTitle() {
+        return mTitle;
+    }
+}
