7e32819147884fc30513c82f02c57b99d5b00d97
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-07-21 02:08:26 +0200

    Build: JSON Parser

--- app/src/main/java/com/example/dev/MainActivity.java
+++ app/src/main/java/com/example/dev/MainActivity.java
@@ -3,12 +3,65 @@ package com.example.dev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private Adapter mAdapter;
+    private ArrayList<Item> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mList = new ArrayList<Item>();
+
+        parseJSON();
+    }
+
+    private void parseJSON() {
+        String url = "http://warrel.net/Dev/api/frontpage";
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for(int i = 0; i < response.length(); i++){
+                    try {
+                        JSONObject dev = response.getJSONObject(i);
+                        String title = dev.getString("title");
+
+                        mList.add(new Item(title));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                mAdapter = new Adapter(MainActivity.this, mList);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
     }
 }
